5b94ef86ef7c9f7a99c5caba0c1193abbd034b3f
Ric4rd1 <111295737+Ric4rd1@users.noreply.github.com>
2022-11-17 22:28:40 -0600

    Update Computadora.h

--- Computadora.h
+++ Computadora.h
@@ -1,57 +1,37 @@
 #include<string>
+#include <iostream>
 using namespace std;
 
-class Computadora {
+class PartyLeader{
     private:
-        string gpu;
-        string cpu;
-        int psu;
-        int ram; //Gb
-        int almacenamiento; //Gb
-        
+        string name;
+        string classification;
+        string effect;
     public:
-        //Constructor
-        Computadora(string _gpu, string _cpu, int _psu, int _ram, int _almacenamiento){
-            gpu = _gpu;
-            cpu = cpu;
-            psu = _psu;
-            ram = _ram;
-            almacenamiento = _almacenamiento;
-        
-        }
-        //Setters
-        void setGpu(string _gpu){
-            gpu = _gpu;
-        }
-        void setCpu (string _cpu){
-            cpu = _cpu;
-        }
-        void setPsu(int _psu){
-            psu = _psu;
-        }
-        void setRam(int _ram){
-            ram = _ram;
-        }
-        void setAlmacenamiento(int _almacenamiento){
-            almacenamiento = _almacenamiento;
+        //constructor
+        PartyLeader(string _name, string _classification, string _effect){
+            name = _name;
+            classification = _classification;
+            effect = _effect;
         }
         //Getters
-        string getGpu(){
-            return gpu;
+        string getName (){
+            return name;
         }
-        string getCpu(){
-            return cpu;
+        string getClassification (){
+            return classification;
         }
-        int getPsu(){
-            return psu;
+        string getEffect(){
+            return effect;
         }
-        int getRam(){
-            return ram;
+        //Methods
+        void applyEffect(){
+            cout<<"Effect: "<<effect<<endl;
         }
-        int getAlmacenamiento(){
-            return almacenamiento;
+        void showPartyLeader (){
+            cout<<"Name: "<<name<<endl;
+            cout<<"Class: "<<classification<<endl;
+            cout<<"Ability: "<<effect<<endl;
+            cout<<"\n";
         }
-        
-       
 };
-
