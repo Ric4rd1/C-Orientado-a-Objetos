a5575d5e6adf589910cb033e06fe46a96e85a300
Ric4rd1 <111295737+Ric4rd1@users.noreply.github.com>
2022-11-28 18:25:55 -0600

    Create Item.h

---
+++ Item.h
@@ -0,0 +1,51 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Item {
+    private:
+        string name;
+        string effect;
+        int quantity;
+        bool inHand  = true;
+    public:
+        //Constructor
+        Item(string _name, string _effect, int _quantity) {
+            name = _name;
+            effect = _effect;
+            quantity = _quantity;
+            inHand = true;
+        }
+        //Getters
+        string getName() {
+            return name;
+        }
+        string getEffect() {
+            return effect;
+        }
+        bool getInHand() {
+            return inHand;
+        }
+        int getQuantity() {
+            return quantity;
+        }
+        //Setters
+        void setName(string _name) {
+            name = _name;
+        }
+        void setEffect(string _effect) {
+            effect = _effect;
+        }
+        void setInHand(bool _inHand) {
+            inHand = _inHand;
+        }
+        void setQuantity(int _quantity) {
+            quantity = _quantity;
+        }
+        //Methods
+        void showItem() {
+            cout << "Name: " << name << endl;
+            cout << "Effect: " << effect << endl;
+        }
+};
