dacdc66c1f5ae608d451feeb5134884d70079dcd
Ric4rd1 <111295737+Ric4rd1@users.noreply.github.com>
2022-12-02 22:11:00 -0600

    Entrega Final

--- Magic.h
+++ Magic.h
@@ -10,33 +10,24 @@ using namespace std;
 class Magic: public Card {
     private:
         string effect;
-        int quantity;
     public:
         //Constructor
-        Magic(string _name, string _effect, int _quantity):Card(_name){
+        Magic(string _name, string _effect):Card(_name){
             effect = _effect;
-            quantity = _quantity;
         }
         //Getters
         string getEffect() {
             return effect;
         }
-        int getQuantity() {
-            return quantity;
-        }
         //Setters
         void setEffect(string _effect) {
             effect = _effect;
-        }
-        void setQuantity(int _quantity) {
-            quantity = _quantity;
         }
         //Methods
         void showCard() {
             cout<<"--------MAGIC--------"<<endl;
             cout << "Name: " << name << endl;
             cout << "Effect: " << effect << endl;
-            cout << "Quantity: " << quantity << endl;
         }  
 };
 
